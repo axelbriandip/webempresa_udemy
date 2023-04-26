7c017dca42ce15379dc205528c2a9d3ddf3ef407
axelbriandip <axelbriandip.rg@gmail.com>
2023-04-26 00:26:58 -0300

    ordering pages done

--- pages/models.py
+++ pages/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(max_length=100, verbose_name='Título')
     content = models.TextField(verbose_name='Contenido')
+    order = models.SmallIntegerField(verbose_name='Orden', default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name='Creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Últ. edición')
 
     class Meta:
         verbose_name = 'Página'
         verbose_name_plural = 'Páginas'
-        ordering = ['-title']
+        ordering = ['order']
 
     def __str__(self):
         return self.title
