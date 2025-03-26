acdabd9b0818729b0c476fcf795c5c3b511e54a7
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-03-26 04:24:46 -0300

    order add

--- pages/models.py
+++ pages/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(verbose_name="título", max_length=200)
     content = models.TextField(verbose_name="contenido")
+    order = models.SmallIntegerField(verbose_name="orden", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Últ. fecha de edición")
 
     class Meta:
         verbose_name = "página"
         verbose_name_plural = "páginas"
-        ordering = ['-created']
+        ordering = ['order', 'title']
 
     def __str__(self):
         return self.title
