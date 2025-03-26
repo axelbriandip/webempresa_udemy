b6e938f3fba0c4477be092cfb029c2ca289b1588
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-03-26 02:39:41 -0300

    filter posts for categories done

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_list_or_404
+from .models import Post, Category
 
 # Create your views here.
 def blog(request):
     posts = Post.objects.all()
     return render(request, 'blog/blog.html', { 'posts': posts })
+
+def cateogory(request, category_id):
+    category = Category.objects.get(id=category_id)
+    posts = Post.objects.filter(categories=category)
+    return render(request, 'blog/category.html', { 'posts': posts })
