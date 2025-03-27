844bb8874519badd5d22f2864b7dfff887e4ed4b
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-03-26 22:23:17 -0300

    users groups add

--- social/admin.py
+++ social/admin.py
@@ -6,4 +6,10 @@ class LinkAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
     list_display = ('name', 'url', 'key')
 
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name="Personal").exists():
+            return ('created', 'updated', 'key', 'name')
+        else:
+            ('created', 'updated')
+
 admin.site.register(Link, LinkAdmin)
